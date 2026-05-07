432a1e50d378c7b29fd707e0e333838c0ca8ae91
yuuksibunta <idayuuki.tiu@gmail.com>
2026-05-07 14:34:41 +0900

    feat: Viteのポートを5173に固定 (#12)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/api': 'http://127.0.0.1:8080',
     },
